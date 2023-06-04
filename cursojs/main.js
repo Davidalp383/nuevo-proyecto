a58aeb6968c29c94739d5f1f18c35f66ad55f934
David lp <dalejol383@gmail.com>
2023-06-04 18:09:36 -0500

    Este es el index de mi proyecto

---
+++ cursojs/main.js
@@ -0,0 +1,13 @@
+const h1 = document.querySelector('h1');
+const input1 = document.querySelector('#input1');
+const input2 = document.querySelector('#input2');
+const btn = document.querySelector('#btnCalcular');
+const result = document.querySelector('#result')
+
+// btn.addEventListener('click', btnOnClick);
+
+
+// function btnOnClick() {
+//     let suma = input1.value + input2.value;
+//     result.innerHTML = suma;
+// }
---
+++ cursojs/prueba.js
@@ -0,0 +1,7 @@
+let person = {
+    name: 'david',
+    lastName: 'lopez',
+    age: 19
+}
+
+console.log(Object.values(person))
